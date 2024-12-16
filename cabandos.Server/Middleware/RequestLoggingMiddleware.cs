64985c4697a2d06e7bb2fc43070f6f01d1435be0
sialiaŭkaivan <selyavkosh2@gmail.com>
2024-12-16 17:34:31 +0300

    logger

---
+++ cabandos.Server/Middleware/RequestLoggingMiddleware.cs
@@ -0,0 +1,23 @@
+﻿namespace cabandos.Server.Middleware;
+
+public class RequestLoggingMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly ILogger<RequestLoggingMiddleware> _logger;
+
+    public RequestLoggingMiddleware(RequestDelegate next, ILogger<RequestLoggingMiddleware> logger)
+    {
+        _next = next;
+        _logger = logger;
+    }
+
+    public async Task InvokeAsync(HttpContext httpContext)
+    {
+        var request = httpContext.Request;
+        _logger.LogInformation($"Request: {request.Method} {request.Path} at {DateTime.UtcNow}");
+
+        await _next(httpContext);
+
+        _logger.LogInformation($"Response: {httpContext.Response.StatusCode} at {DateTime.UtcNow}");
+    }
+}
