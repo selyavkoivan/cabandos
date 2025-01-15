9442ac04f21961799e4c140fedf9c0b2c828ac82
sialiaŭkaivan <selyavkosh2@gmail.com>
2025-01-15 18:05:54 +0300

    find users handlers

---
+++ cabandos.Server/Features/Users/Handlers/GetMeHandler.cs
@@ -0,0 +1,31 @@
+﻿using cabandos.Server.Data;
+using MediatR;
+using cabandos.Server.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+using cabandos.Server.Migrations;
+
+namespace cabandos.Server.Features.Users.Handlers;
+
+public class GetMeHandler : IRequestHandler<GetMeQuery, User?>
+{
+    private UserManager<User> _userManager;
+
+    public GetMeHandler(UserManager<User> userManager)
+    {
+        _userManager = userManager;
+    }
+
+    public async Task<User?> Handle(GetMeQuery request, CancellationToken cancellationToken)
+    {
+        return await this._userManager.GetUserAsync(request.User);
+    }
+}
+
+public class GetMeQuery : IRequest<User?>
+{
+    public ClaimsPrincipal User { get; }
+
+    public GetMeQuery(ClaimsPrincipal user) => User = user;
+}
---
+++ cabandos.Server/Features/Users/Handlers/GetUserByUsername.cs
@@ -0,0 +1,28 @@
+﻿using cabandos.Server.Data;
+using MediatR;
+using cabandos.Server.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace cabandos.Server.Features.Users.Handlers;
+
+public class GetUserByUsernameHandler : IRequestHandler<GetUserByUsernameQuery, User?>
+{
+    private ApplicationContext _context;
+
+    public GetUserByUsernameHandler(ApplicationContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<User?> Handle(GetUserByUsernameQuery request, CancellationToken cancellationToken)
+    {
+        return await _context.Users.FirstOrDefaultAsync(u => u.UserName == request.Username);
+    }
+}
+
+public class GetUserByUsernameQuery : IRequest<User?>
+{
+    public string Username { get; }
+
+    public GetUserByUsernameQuery(string username) => Username = username;
+}
