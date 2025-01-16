68de30fde1dc052470342c1447dc9cb34b5605e9
sialiaŭkaivan <selyavkosh2@gmail.com>
2025-01-16 18:19:19 +0300

    set 'User' role for new Users

--- cabandos.Server/Features/Auth/Handlers/SignUpHandler.cs
+++ cabandos.Server/Features/Auth/Handlers/SignUpHandler.cs
@@ -4,35 +4,56 @@ using MediatR;
 using Microsoft.AspNetCore.Identity;
 using cabandos.Server.Domain.Entities;
 
-namespace cabandos.Server.Features.Auth.Handlers;
-
-public class SignUpHandler : IRequestHandler<SignUpCommand, IdentityResult>
+namespace cabandos.Server.Features.Auth.Handlers
 {
-    private readonly UserManager<User> _userManager;
-
-    public SignUpHandler(UserManager<User> userManager)
+    public class SignUpHandler : IRequestHandler<SignUpCommand, IdentityResult>
     {
-        _userManager = userManager;
+        private readonly UserManager<User> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public SignUpHandler(UserManager<User> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        public async Task<IdentityResult> Handle(SignUpCommand request, CancellationToken cancellationToken)
+        {
+            var user = new User(request.UserDto);
+            var result = await _userManager.CreateAsync(user, request.UserDto.Password!);
+
+            if (result.Succeeded)
+            {
+                var roleExists = await _roleManager.RoleExistsAsync("User");
+                if (!roleExists)
+                {
+                    var roleCreationResult = await _roleManager.CreateAsync(new IdentityRole("User"));
+                    if (!roleCreationResult.Succeeded)
+                    {
+                        throw new AuthException("RoleCreationFailed", roleCreationResult);
+                    }
+                }
+
+                var addRoleResult = await _userManager.AddToRoleAsync(user, "User");
+                if (!addRoleResult.Succeeded)
+                {
+                    throw new AuthException("AddRoleFailed", addRoleResult);
+                }
+
+                return result;
+            }
+
+            throw new AuthException("SignUpFailed", result);
+        }
     }
 
-    public async Task<IdentityResult> Handle(SignUpCommand request, CancellationToken cancellationToken)
+    public class SignUpCommand : IRequest<IdentityResult>
     {
-        var user = new User(request.UserDto);
-        var result = await _userManager.CreateAsync(user, request.UserDto.Password!);
+        public UserDTO UserDto { get; }
 
-        if (result.Succeeded)
-            return result;
-
-        throw new AuthException("SignInFailed", result);
-    }
-}
-
-public class SignUpCommand : IRequest<IdentityResult>
-{
-    public UserDTO UserDto { get; }
-
-    public SignUpCommand(UserDTO userDto)
-    {
-        UserDto = userDto;
+        public SignUpCommand(UserDTO userDto)
+        {
+            UserDto = userDto;
+        }
     }
 }
