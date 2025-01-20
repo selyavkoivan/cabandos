3a6d00b7b347d4631f12aaa4c81e6d95c9849ebe
sialiaŭkaivan <selyavkosh2@gmail.com>
2025-01-20 15:42:12 +0300

    change task component style

--- cabandos.client/src/components/kanban/Task.jsx
+++ cabandos.client/src/components/kanban/Task.jsx
@@ -1,7 +1,8 @@
 ﻿import React, { Component } from 'react';
-import { Card, CardBody, CardTitle, CardText, Row, Col, Button } from 'reactstrap';
+import { Card, CardBody, CardTitle, CardText } from 'reactstrap';
 import { AiOutlineClose } from "react-icons/ai";
-import "../../assets/styles/Board.css"
+import "../../assets/styles/Board.css";
+
 class Task extends Component {
     constructor(props) {
         super(props);
@@ -12,50 +13,52 @@ class Task extends Component {
     };
 
     handleDelete = (event) => {
-        this.props.onDeleteTask(this.props.task)
-    }
+        this.props.onDeleteTask(this.props.task);
+    };
 
     render() {
-        const { task } = this.props;
-
+        const { task, isFreezed } = this.props;
         return (
-            <Card key={task.id} className="m-1">
+            <Card key={task.id} className="m-1 hoverable">
                 <CardTitle
                     className="d-flex align-items-center justify-content-between"
                     style={{
                         height: '30px',
                         cursor: 'grab',
-                        padding: '0 10px'
+                        padding: '0 10px',
                     }}
                     draggable
-                    onDragStart={this.handleDragStart}
+                    onDragStart={!isFreezed && this.handleDragStart}
                     tag="h5"
                 >
                     <span style={{ flex: 1, textAlign: 'center' }}>{task.name}</span>
-                    <AiOutlineClose
-                        style={{
-                            marginLeft: 'auto',
-                            cursor: 'pointer',
-                        }}
-                        onDragStart={e => e.stopPropagation()}
-                        onClick={this.handleDelete}
-                    />
-
+                    {!isFreezed && (
+                        <AiOutlineClose
+                            className="ai-outline-close"
+                            onDragStart={(e) => e.stopPropagation()}
+                            onClick={this.handleDelete}
+                        />
+                    )}
                 </CardTitle>
                 <CardBody>
-                    <Row>
-                        <Col xs="11">
-                            <CardText className="p-2 mb-2 text-center">
-                                Name: {task.name}
-                            </CardText>
-                            <CardText className="p-2 mb-2 text-center card-text">
-                                Description: {task.description}
-                            </CardText>
-                            <CardText className="p-2 mb-2 text-center">
-                                Status: {task.status}
-                            </CardText>
-                        </Col>
-                    </Row>
+                    <CardText className="p-2 mb-2 text-center name">
+                        Name: {task.name}
+                    </CardText>
+                    <CardText className="p-2 mb-2 text-center card-text">
+                        Description: {task.description || 'Not provided'}
+                    </CardText>
+                    {task.userName && (
+                        <CardText className="p-2 mb-2 text-center card-text">
+                            Creator: {task.userName}
+                        </CardText>
+                    )}
+                    <CardText
+                        className={`p-2 mb-2 text-center card-text status ${task.status !== undefined && `status-${task.status}`}`}
+
+
+                    >
+                        Status: {task.status}
+                    </CardText>
                 </CardBody>
             </Card>
         );
