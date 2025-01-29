c33eff41cbb1e02fc91ca0d21d621e33864c0a3e
sialiaŭkaivan <selyavkosh2@gmail.com>
2025-01-29 15:59:51 +0300

    Refactor: non authorize user can get profile

--- cabandos.Server/Controllers/UserController.cs
+++ cabandos.Server/Controllers/UserController.cs
@@ -7,7 +7,6 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace cabandos.Server.Controllers;
 
-[Authorize]
 [ApiController]
 [Route("api/user")]
 public class UserController : ControllerBase
@@ -19,15 +18,21 @@ public class UserController : ControllerBase
         _mediator = mediator;
     }
 
+
+    [Authorize]
     [HttpPost("search")]
     public async Task<IActionResult> SearchUsers([FromBody] SearchUsersDTO searchUserDTO) =>
         Ok(await _mediator.Send(new SearchUsersQuery(searchUserDTO)));
 
+
+    [Authorize]
     [HttpGet("all")]
     public async Task<IActionResult> GetAllUsers() =>
         Ok(await _mediator.Send(new SearchUsersQuery(new SearchUsersDTO
         { IncludeRoles = true })));
 
+
+    [Authorize]
     [HttpPost("me")]
     public async Task<IActionResult> GetMe([FromBody] SearchUsersDTO searchUserDTO) =>
         Ok(await _mediator.Send(new GetMeQuery(HttpContext.User, searchUserDTO)));
@@ -37,10 +42,14 @@ public class UserController : ControllerBase
         Ok(await _mediator.Send(new GetUserByUsernameQuery(
             new SearchUsersDTO (username, true, true))));
 
+
+    [Authorize]
     [HttpPost("profile")]
     public async Task<IActionResult> GetUser([FromBody] SearchUsersDTO searchUserDTO) =>
         Ok(await _mediator.Send(new GetUserByUsernameQuery(searchUserDTO)));
 
+
+    [Authorize]
     [HttpPost("edit/userdata/{username}")]
     public async Task<IActionResult> EditUser(string username, [FromBody] UserDTO userDTO)
     {
@@ -48,6 +57,8 @@ public class UserController : ControllerBase
         return NoContent();
     }
 
+
+    [Authorize]
     [HttpPost("edit/password/{username}")]
     public async Task<IActionResult> EditUserPassword(string username, [FromBody] UserDTO userDTO)
     {
@@ -55,6 +66,8 @@ public class UserController : ControllerBase
         return NoContent();
     }
 
+
+    [Authorize]
     [HttpPost("edit/avatar/{userName}")]
     public async Task<IActionResult> UploadAvatar(string userName, IFormFile file)
     {
