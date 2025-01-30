deb1d12bcb4d5137064853f0f7501f158a349994
sialiaŭkaivan <selyavkosh2@gmail.com>
2025-01-30 15:35:37 +0300

    add methods for fetching task change history

--- cabandos.client/src/redux/slice/task/taskSlice.jsx
+++ cabandos.client/src/redux/slice/task/taskSlice.jsx
@@ -1,14 +1,22 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import { taskApi } from './taskApi'; 
+import { taskApi } from './taskApi';
 
 import allowedMoves from '../../../assets/allowedMoves.json';
 import taskStatusMap from "../../../assets/taskStatus.json";
+
+export const fetchTaskChangesAsync = createAsyncThunk(
+    'task/fetchTaskChanges',
+    async (taskId) => {
+        const data = await taskApi.fetchTaskChanges(taskId);
+        return data;
+    }
+);
 
 export const fetchTasksByStatusAsync = createAsyncThunk(
     'task/fetchTasksByStatus',
     async () => {
         const data = await taskApi.fetchTasksByStatus();
-        return data; 
+        return data;
     }
 );
 
@@ -43,6 +51,7 @@ const initialState = {
     addingTaskStatus: false,
     allowedMoves,
     taskStatusMap,
+    taskChanges: [], 
 };
 
 const taskSlice = createSlice({
@@ -52,6 +61,9 @@ const taskSlice = createSlice({
         setTasksGroup: (state, action) => {
             state.tasksGroup = action.payload;
         },
+        setTaskChanges: (state, action) => {
+            state.taskChanges = action.payload; 
+        }
     },
     extraReducers: (builder) => {
         builder
@@ -66,8 +78,8 @@ const taskSlice = createSlice({
             })
             .addCase(moveTaskAsync.fulfilled, (state, action) => {
                 const taskDTO = action.payload;
-                const task = taskDTO.task
-                const toStatus = taskDTO.toStatus
+                const task = taskDTO.task;
+                const toStatus = taskDTO.toStatus;
                 const tasksGroup = [...state.tasksGroup];
 
                 const tasksToDelete = tasksGroup[Math.ceil(task.status / 10)].tasks.find(t => t.status == task.status).tasks;
@@ -78,7 +90,7 @@ const taskSlice = createSlice({
                 }
 
                 const tasksToAdd = tasksGroup[Math.ceil(toStatus / 10)].tasks.find(t => t.status == toStatus).tasks;
-                tasksToAdd.push({ ...task, status: toStatus })
+                tasksToAdd.push({ ...task, status: toStatus });
             })
             .addCase(deleteTaskAsync.fulfilled, (state, action) => {
                 const task = action.payload;
@@ -88,10 +100,13 @@ const taskSlice = createSlice({
                 if (index !== -1) {
                     tasks.splice(index, 1);
                 }
+            })
+            .addCase(fetchTaskChangesAsync.fulfilled, (state, action) => {
+                state.taskChanges = action.payload;
             });
     },
 });
 
-export const { setTasksGroup } = taskSlice.actions;
+export const { setTasksGroup, setTaskChanges } = taskSlice.actions;
 
 export default taskSlice.reducer;
