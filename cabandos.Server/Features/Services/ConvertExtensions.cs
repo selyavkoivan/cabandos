58746d00f48499d8c5b0979529741ba8841277ca
sialiaŭkaivan <selyavkosh2@gmail.com>
2025-02-06 17:20:01 +0300

    Feat: Add extension method to convert object to string, including TaskStatus specifically

---
+++ cabandos.Server/Features/Services/ConvertExtensions.cs
@@ -0,0 +1,48 @@
+﻿using cabandos.Server.Domain.Entities;
+using TaskStatus = cabandos.Server.Domain.Entities.TaskStatus;
+
+namespace cabandos.Server.Features.Services;
+
+public static class ConvertExtensions
+{
+    public static string GetStatusName(this TaskStatus status)
+    {
+        return status switch
+        {
+            TaskStatus.NotStarted => "Not Started",
+            TaskStatus.InProgress => "In Progress",
+            TaskStatus.Completed => "Completed",
+            TaskStatus.Canceled => "Canceled",
+            _ => status.ToString()
+        };
+    }
+
+    public static string ConvertValueToString(this object value)
+    {
+        if (value is null || string.IsNullOrEmpty(value as string))
+        {
+            return "No Data";
+        }
+
+        if (value is TaskStatus status)
+        {
+            return status.GetStatusName();
+        }
+
+
+        if (value is IConvertible)
+        {
+            try
+            {
+                return Convert.ToString(value);
+            }
+            catch
+            {
+                return value.ToString();
+            }
+        }
+
+        return value.ToString();
+    }
+}
+
